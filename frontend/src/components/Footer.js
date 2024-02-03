82671ebf9fba8154c30fe11f24dfa1772926a9d9
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-02-03 22:49:03 +0300

    mest1o

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+      <p className="footer__copyright">&copy; 2023 Mesto Russia</p>
     </footer>
   );
 }
